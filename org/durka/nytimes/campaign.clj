702eb24cf22768c1587e4a7b6d736f42532bf97f
Alex Burka <aeq3mno02@sneakemail.com>
2009-01-16 01:12:03 -0500

    added files

---
+++ org/durka/nytimes/campaign.clj
@@ -0,0 +1,45 @@
+(ns org.durka.nytimes.campaign
+  (:use org.durka.nytimes.common))
+
+(def *version* "v2")
+
+(def *api-key* (:campaign api-key))
+
+(defn prelude
+  [type year]
+  ["svc" "elections" "us" *version* type year "finances"])
+
+(defn summaries
+  "Get financial summaries for all candidates in a particular election."
+  [type year]
+  (:results (request (concat (prelude type year) ["candidates"])
+                     {}
+                     *api-key*)))
+
+(defn details
+  "Get details for a specific candidate in a given election. candidate can be a string (last name) or ID."
+  [type year candidate]
+  (:results (request (concat (prelude type year) ["candidates" candidate])
+                                 {}
+                                 *api-key*)))
+
+(defn by-state
+  "Total donations to each candidate from a state (identified by postal code)."
+  [type year state]
+  (:results (request (concat (prelude type year) ["states" state])
+                     {}
+                     *api-key*)))
+
+(defn by-zip
+  "Total donations to each candidate from a ZIP code (5-digit)."
+  [type year zip]
+  (:results (request (concat (prelude type year) ["zips" zip])
+                     {}
+                     *api-key*)))
+
+(defn donor-search
+  "Lazy seq of the results of searching for donors -- the server returns results in pages of 100, so evluation will occur when the 100n+1'th entry is accessed. Restrict by zip code, last name, and/or first name."
+  [type year params]
+  (:results (request (concat (prelude type year) ["contributions" "donorsearch"])
+                     params
+                     *api-key*)))
---
+++ org/durka/nytimes/common.clj
@@ -0,0 +1,24 @@
+(ns org.durka.nytimes.common
+;  (:use clj-http-client.core)
+  (:use clojure.contrib.seq-utils)
+  (:use clojure.contrib.duck-streams)
+  (:require (org.danlarkin [json :as json])))
+
+(def api-key {:campaign   "e19c3c51ad52a90dcb3da6ae991e318e:18:57691504"
+              :movies     "8e8ed7dc4e55e1fa79a05aa458f04962:10:57691504"
+              :community  "c2ztv3hpxc6d4eyxe6tbruqz"
+              :congress   "2c7c0acde7e0ceac62793c728cb2aa89:9:57691504"
+              :tags       "89dde161728357343a36ff5f42785b1e:17:57691504"})
+
+(defn request
+  [dirs params kee]
+  (with-open [nyt (reader
+                    (apply str (flatten
+                                 ["http://api.nytimes.com/"
+                                  (interpose "/" dirs)
+                                  ".json?"
+                                  (interpose "&"
+                                             (map #(str (key %) "=" (val %))
+                                                  (conj {'api-key kee} params)))])))]
+    (json/decode-from-reader nyt)))
+
